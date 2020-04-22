d76b05aa13de49a9ff4e28c5fa9792f264cb4a70
augkik <62946245+augkik@users.noreply.github.com>
2020-04-22 10:18:56 +0300

    Add files via upload

---
+++ contacts/src/main/java/Contacts/application/Application.java
@@ -0,0 +1,21 @@
+package Contacts.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
